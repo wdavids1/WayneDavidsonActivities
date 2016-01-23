64a06eeef007700cd6b53d55202fc3909bc25dca
Wayne Davidson <wdavids1@my.westga.edu>
2016-01-23 15:00:01 -0700

    Added third activity to receive 2 messages

--- app/src/main/java/edu/westga/cs6242/waynedavidsonactivities/DisplayTwoMessagesActivity.java
+++ app/src/main/java/edu/westga/cs6242/waynedavidsonactivities/DisplayTwoMessagesActivity.java
@@ -1,39 +1,20 @@
 package edu.westga.cs6242.waynedavidsonactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.content.Intent;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
-    public final static String NEW_EXTRA_MESSAGE = "edu.westga.cs6242.waynedavidsonactivities.MESSAGE";
-    public final static String NEW_EXTRA_MESSAGE2 = "edu.westga.cs6242.waynedavidsonactivities.MESSAGE2";
-
-    /** Called when the user clicks the next button */
-    public void nextMessage(View view) {
-        Intent intent = new Intent(this, DisplayTwoMessagesActivity.class);
-
-        TextView textView = (TextView) findViewById(R.id.first_message);
-        String message = textView.getText().toString();
-        intent.putExtra(NEW_EXTRA_MESSAGE, message);
-
-        EditText editText = (EditText) findViewById(R.id.edit_second_message);
-        String message2 = editText.getText().toString();
-        intent.putExtra(NEW_EXTRA_MESSAGE2, message2);
-
-        startActivity(intent);
-    }
+public class DisplayTwoMessagesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+        setContentView(R.layout.activity_display_two_messages);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,9 +29,14 @@ public class DisplayMessageActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(DisplayMessageActivity.NEW_EXTRA_MESSAGE);
+        String message2 = intent.getStringExtra(DisplayMessageActivity.NEW_EXTRA_MESSAGE2);
+
         TextView textView = (TextView) findViewById(R.id.first_message);
         textView.setText(message);
+
+        TextView textView2 = (TextView) findViewById(R.id.second_message);
+        textView2.setText(message2);
     }
 
 }
